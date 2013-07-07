bb93f09307b34d61dc9832f819fa9d82335f6f97
Simon Garner <sgarner@expio.co.nz>
2013-07-08 11:44:54 +1200

    Add an autoloader for entity proxy classes
    Enables proper unserialization of entities, e.g. from session authentication.

--- src/id009/Neo4jBundle/id009Neo4jBundle.php
+++ src/id009/Neo4jBundle/id009Neo4jBundle.php
@@ -27,4 +27,36 @@ class id009Neo4jBundle extends Bundle
         	$container->getExtension('security')->addUserProviderFactory(new EntityFactory('neo4j', 'id009_neo4j.security.provider'));
         }
 	}
+
+    /**
+     * {@inheritDoc}
+     */
+    public function boot()
+    {
+        // Register an autoloader for proxies to avoid issues when unserializing them
+        // when the OGM is used. (Based on code from DoctrineBundle)
+
+        $namespace = 'neo4jProxy';
+        $dir = $this->container->getParameter('id009_neo4j.proxy_dir');
+        // See https://github.com/symfony/symfony/pull/3419 for usage of
+        // references
+        $container =& $this->container;
+
+        $this->autoloader = function($class) use ($dir, $namespace, &$container) {
+            if (0 === strpos($class, $namespace)) {
+                $fileName = str_replace('\\', '_', $class);
+                $file = $dir.DIRECTORY_SEPARATOR.$fileName.'.php';
+
+                if (!is_file($file)) {
+                    $realClass = str_replace('_', '\\', substr($class, strlen($namespace)));
+                    $em = $container->get('id009_neo4j.entity_manager');
+                    $em->createProxy($realClass);
+                }
+                else {
+                    require $file;
+                }
+            }
+        };
+        spl_autoload_register($this->autoloader);
+    }
 }
